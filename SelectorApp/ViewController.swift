e66fa4152d4b6f728eafb11d44ef18656dca65e3
Junior <duartejusanca@gmail.com>
2022-01-20 14:05:31 +0100

    first commit

--- SelectorApp/ViewController.swift
+++ SelectorApp/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var slider: UISlider!
+    
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func show(_ sender: Any) {
+        let alert = UIAlertController(title: "value", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Fermer", style : .default, handler: nil)
+        
+        alert.addAction(action)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func slider(_ sender: Any) {
+        if let slid = sender as? UISlider{
+            /*let roudValue = 100*slider.value
+            label.text = "\(roudValue)"
+        */
+            newValue(value: slid.value)
+             }
+    }
+    
+    func newValue(value: Float){
+        let roundedValue = Int(100*value)
+        label.text = "Value: \(roundedValue)"
+    }
+    
 }
 
